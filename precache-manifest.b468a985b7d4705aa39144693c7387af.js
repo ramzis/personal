51e3df97808b73335e14a3e671d0117b6d4f4ab2
Tadas Audinis <ta15668@my.bristol.ac.uk>
2019-06-28 16:31:52 -0700

    Updates

---
+++ precache-manifest.b468a985b7d4705aa39144693c7387af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e218750457082fa0775efb7f92c2d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dac23e691eec85c1a0f",
+    "url": "/static/css/main.ece6c2bd.chunk.css"
+  },
+  {
+    "revision": "3b9f66863f27f726d294",
+    "url": "/static/js/2.5d49121f.chunk.js"
+  },
+  {
+    "revision": "9dac23e691eec85c1a0f",
+    "url": "/static/js/main.6670667d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c38c59d40081b2ae85baca4e091013ff",
+    "url": "/static/media/houses.c38c59d4.jpg"
+  },
+  {
+    "revision": "68753d36ec0c4d7768b20993268d68d1",
+    "url": "/static/media/resume.68753d36.txt"
+  }
+]);
